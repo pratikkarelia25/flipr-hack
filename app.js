394cb53f74b0a5e384dca2bf9c57775e9bea8f62
shashwatha411 <shashwatha411@gmail.com>
2021-06-26 19:49:49 +0530

    Login Authorisation Done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 //Imports & initialisation
 const express = require('express');
-// const expresslayout = require('express-ejs-layouts');
+const expresslayout = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 const app = express();
 const port = 3000;
 require('dotenv').config();
@@ -15,11 +19,36 @@ app.use('/img',express.static(__dirname + 'public/img'));
 
 
 // ######################    LOGIN     #######################################
-
+// Passport Config
+require('./model/passport')(passport);
 //EJS
-// app.use(expresslayout);
+app.use(expresslayout);
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: false}));
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+  // Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+  // Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
 
 //routes
 app.use('/', require('./routes/initial'));
--- routes/initial.js
+++ routes/initial.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res) => res.render('index'));
+
+
+
+const { ensureAuthenticated, forwardAuthenticated } = require('../model/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('index'));
+
+// Dashboard
+router.get('/email/index', ensureAuthenticated, (req, res) =>
+  res.render('index', {
+    user: req.user
+  })
+);
 module.exports = router;
